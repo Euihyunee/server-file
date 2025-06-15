55be529dc5965c7e2014c4940a1e26703296123f
euihyunee <lobil7782@gmail.com>
2025-06-15 18:05:56 +0900

    [add] ex file

--- ex.sql
+++ ex.sql
@@ -16,8 +16,8 @@
 
 
 -- test 데이터베이스 구조 내보내기
-CREATE DATABASE IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `test`;
+CREATE DATABASE IF NOT EXISTS `feature_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `feature_test`;
 
 -- 테이블 test.alarm 구조 내보내기
 CREATE TABLE IF NOT EXISTS `alarm` (
